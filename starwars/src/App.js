88ee55df192e28dde9406a4df18d74d1e2513879
Mannie <carlos.alvarezberrios@gmail.com>
2020-04-18 14:53:40 -0400

    Completed MVP and Pagination Stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Container, Row, Col, Spinner } from 'reactstrap';
+import Character from "./components/Character";
+import Pages from "./components/Pages";
+import pokemon from "./images/pokemon_logo.png";
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+function App() {
+  const [pokemonData, setPokemonData] = useState({});
+  const [page, setPage] = useState("");
 
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${page}`)
+      .then(response => {
+        //console.log(response.data.pokemon_entries);
+        setPokemonData(response.data)
+        /* setNextPage(response.data.next.slice(response.data.next.indexOf("?")));
+        setPrevPage(response.data.previous.slice(response.data.next.indexOf("?"))); */
+      })
+      .catch(err => console.log("Error getting list", err));
+  }, [page]);
+
+  
+  
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="container">
+      
+      <h1>
+        <img src = {pokemon} alt = "Pokemon Logo" width = "60%"  />
+      </h1>
+      <Pages pokemonData = {pokemonData} setOffset = {setPage} />
+      <Container>
+        <Row xs = "1" sm = "2" md = "3" lg = "4">
+          
+            {pokemonData.results ? pokemonData.results.map((pokemon, index) => (
+              <Col key = {index}>
+                <Character pokemon = {pokemon} />
+              </Col>
+            )) : <h2 style = {{margin: "0 auto"}}>Loading <Spinner /></h2>}
+          
+          </Row>
+      </Container>
+      <Pages pokemonData = {pokemonData} setOffset = {setPage} />
     </div>
   );
 }
---
+++ starwars/src/components/Pages.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from "reactstrap";
+import styled from "styled-components";
+
+const PagesBtns = styled.div`
+    text-align: center;
+    margin: 30px 0;
+
+    button {
+        width: 100px;
+    }
+`;
+
+
+const Pages = ({ pokemonData, setOffset }) => {
+
+    return (
+        <PagesBtns className = "page_nav">
+            <Button color="danger" onClick = {() => setOffset("")}>First</Button>{' '}
+            <Button color="danger" onClick = {() => pokemonData.previous ? setOffset(pokemonData.previous.slice(pokemonData.previous.indexOf("?"))) : null}>Prev</Button>{' '}
+            <Button color="danger" onClick = {() => pokemonData.next ? setOffset(pokemonData.next.slice(pokemonData.next.indexOf("?"))) : null}>Next</Button>{' '}
+            <Button color="danger" onClick = {() => setOffset(`?offset=${pokemonData.count - 20}&limit=20`)}>Last</Button>
+        </PagesBtns>
+    );
+};
+
+export default Pages;
